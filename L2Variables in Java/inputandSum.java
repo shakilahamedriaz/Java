4aeed3e7d8193c8bbdf90660e4a90875f2250c8b
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-01 20:50:23 +0600

    great start

---
+++ L2Variables in Java/inputandSum.java
@@ -0,0 +1,16 @@
+package shakil;
+import java.util.*;
+
+public class inputandSum {
+    public static void main(String[] agrs){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+
+        int sum= a+b;
+
+        System.out.println(sum);
+    }
+}
---
+++ L2Variables in Java/iput.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class iput {
+   
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+
+        String name= sc.next();
+        System.out.println(name);
+        //nextInt()
+        //nextFLoat()
+        //nextFloat()
+        //nextdouble()
+        //etc
+        //sc.nextLIne()
+
+        String fullName = sc.nextLine();
+        System.out.println(fullName);
+
+    }
+}
