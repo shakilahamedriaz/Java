aa8ee0b86924a9a285aad80d56c99562c2aa988a
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-01 21:23:50 +0600

    done

---
+++ L3ConditionalStatements/evenOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class evenOdd {
+    public static void main(String[] args){
+
+     Scanner sc= new Scanner(System.in);
+    int x= sc.nextInt();
+
+    if(x%2==0){
+
+        System.out.println("Even Number");
+    }
+    else{
+
+        System.out.println("Odd");
+    }
+
+    }
+}
