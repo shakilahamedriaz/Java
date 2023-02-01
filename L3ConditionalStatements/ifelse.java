68f26316aa66e934c6b625eb579d3ef6da37f43e
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-01 21:20:23 +0600

    done

---
+++ L3ConditionalStatements/ifelse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ifelse{
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+        int age= sc.nextInt();
+
+        if(age>18){
+
+            System.out.println("Audult");
+        }
+        else{
+
+            System.out.println("Not Audult");
+        }
+}    }
