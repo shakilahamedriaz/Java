b339f210af6ddbe09bacc40835042699f09617a0
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-08 15:37:38 +0600

    done

---
+++ L4LoopsInJava/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args){
+
+        int i=0;;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
