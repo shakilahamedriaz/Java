8142cb9b327aa5cec53841028106ecbf40efa51f
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-08 15:41:56 +0600

    Done

---
+++ L4LoopsInJava/dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+    public static void main(String[] args){
+       
+         //atleast one time execution
+        int i=0;
+        do{
+              System.out.println(i);  
+              i++;     
+        }while(i<11);
+    }
+}
