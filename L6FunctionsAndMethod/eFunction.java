1f304fbd591791311f6b6de07c3a57fd5202aabe
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-03-08 17:13:16 +0600

    done

--- L6FunctionsAndMethod/eFunction.java
+++ L6FunctionsAndMethod/eFunction.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class eFunction {
     public static void printFactorial(int n){
-        //loop
+        //loop for factorial.
+        if(n<0){
+            System.out.println("Invalid Number !");
+            return;
+        }
          int factorial=1;
         for(int i=n; i>=1; i--){
            
-            factorial=factorial*i;
+            factorial*=i;
 
         }
         System.out.print(factorial);
         return ;
-        
     }
     public static void main(String[] args){
+
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n=sc.nextInt();
