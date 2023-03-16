100a5bef7c44d31db616d5cb95ad7d3d4ced52f9
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-03-17 01:47:42 +0600

    java time

---
+++ L7ArraysIntroduction/aDefiningArray.java
@@ -0,0 +1,19 @@
+package L7ArraysIntroduction;
+
+public class aDefiningArray {
+    
+
+    public static void main(String[] args){
+
+    int[] marks= new int[3];            //defining an array:
+                                        //type[] arrayName=new type[size];
+    marks[0]=97; //phy
+    marks[1]=55; //che
+    marks[2]=88; //eng
+
+    System.out.println(marks[0]);
+    System.out.println(marks[1]);
+    System.out.println(marks[2]);
+
+    }
+}
